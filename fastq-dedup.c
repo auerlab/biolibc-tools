ff3d5870e462cb41cb417ddbc3131ba11b63ca60
Jason Bacon <bacon@danio.uits.uwm.edu>
2021-08-18 19:04:00 -0500

    fastq-dedup: Fix design flaw allocating structures for duplicate keys

--- fastq-dedup.c
+++ fastq-dedup.c
@@ -3,6 +3,11 @@
  *      Remove duplicates from a fastq stream.  This implementation is
  *      similar to seqkit rmdup, using xxhash to generate hash keys and
  *      uthash to build a hash table and check for duplicates.
+ *
+ *      Currently this implementation has the same runtime as seqkit rmdup,
+ *      uses significantly less memory, but does not filter all the same
+ *      records.  seqkit appears to filter the correct number of records
+ *      based on agreement with the sort - uniq method.
  *  
  *  History: 
  *  Date        Name        Modification
@@ -32,21 +37,27 @@ int     main(int argc, char *argv[])
     entry_t         *table = NULL,
 		    *entry,
 		    *found = NULL;
+    XXH64_hash_t    hash;
     
     records_read = records_written = 0;
     while ( bl_fastq_read(stdin, &rec) == BL_READ_OK )
     {
 	++records_read;
-	entry = xt_malloc(1, sizeof(entry_t));
-	entry->hash = XXH64(BL_FASTQ_SEQ(&rec), BL_FASTQ_SEQ_LEN(&rec), seed);
-	HASH_FIND_INT(table, &entry->hash, found);
+	hash = XXH64(BL_FASTQ_SEQ(&rec), BL_FASTQ_SEQ_LEN(&rec), seed);
+	HASH_FIND(hh, table, &hash, sizeof(hash), found);
 	if ( found == NULL )
 	{
-	    HASH_ADD_INT(table, hash, entry);
-	    ++records_written;
+	    // Record key for comparison to future records
+	    entry = xt_malloc(1, sizeof(entry_t));
+	    entry->hash = hash;
+	    HASH_ADD(hh, table, hash, sizeof(entry->hash), entry);
+	    
+	    // Output record
 	    bl_fastq_write(stdout, &rec, BL_FASTQ_LINE_UNLIMITED);
+	    ++records_written;
 	}
     }
+    
     fprintf(stderr, "%zu records read, %zu written, %zu removed\n",
 	   records_read, records_written, records_read - records_written);
     return EX_OK;

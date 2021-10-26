0ed7ee3d27139baf3bc65f80aef8fbe39d230df1
Jason Bacon <bacon@moray.acadix.biz>
2021-10-26 08:39:33 -0500

    find-orfs: Find subsequent ORFs to EOF (no intervening codons yet) Generate man pages for new programs

--- find-orfs.c
+++ find-orfs.c
@@ -1,25 +1,73 @@
 #include <stdio.h>
 #include <sysexits.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 #include <biolibc/translate.h>
+
+void    usage(char *argv[]);
 
 int     main(int argc,char *argv[])
 
 {
-    long    pos = 1,
-	    offset;
-    char    codon[4];
+    long        pos,
+		offset,
+		c;
+    char        codon[4],
+		*end;
+    struct stat st;
+    FILE        *stream = stdin;
+    
+    if ( argc != 2 )
+	usage(argv);
 
-    if ( (offset = next_start_codon(stdin)) != EOF )
+    pos = strtol(argv[1], &end, 10);
+    if ( *end != '\0' )
     {
-	printf("Start codon AUG at %ld (+%ld)\n", pos + offset, offset);
-	pos += offset + 3;
-	if ( (offset = next_stop_codon(stdin, codon)) != EOF )
-	    printf("Stop codon %s at %ld (+%ld)\n", codon, pos + offset, offset);
-	else
-	    puts("No stop codon found.");
+	fprintf(stderr, "Invalid offset: %s\n", argv[1]);
+	usage(argv);
+    }
+    ++pos;  // Make 1-based
+    
+    fstat(fileno(stream), &st);
+    if ( ! (st.st_mode & S_IFIFO) )
+    {
+	if ( fseek(stream, pos - 1, SEEK_CUR) == -1 )
+	{
+	    fputs("Seek failed.", stderr);
+	    return EX_SOFTWARE;
+	}
     }
     else
-	puts("No start codon found.");
+    {
+	for (c = 0; c < pos - 1; ++c)
+	    getc(stream);
+    }
+    while ( ! feof(stream) )
+    {
+	if ( (offset = next_start_codon(stream, codon)) != EOF )
+	{
+	    printf("Start codon %s at %ld (+%ld)\n", codon, pos + offset, offset);
+	    pos += offset + 3;
+	    if ( (offset = next_stop_codon(stream, codon)) != EOF )
+	    {
+		printf("Stop codon %s at %ld (+%ld)\n", codon, pos + offset, offset);
+		pos += offset + 3;
+	    }
+	    else
+		puts("No stop codon found.");
+	}
+	else
+	    puts("No start codon found.");
+    }
     return EX_OK;
+}
+
+
+void    usage(char *argv[])
+
+{
+    fprintf(stderr, "Usage: %s offset\n(offset is 0-based)\n",
+	    argv[0]);
+    exit(EX_USAGE);
 }
